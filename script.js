724c29325169c0eb5802a7adc3cbee1f51b0b26d
Novella C <novella@altodev.net>
2019-07-05 19:40:15 -0400

    Better working js, similified html

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-/**
- * Part 0. Click the button
- */
+// https://www.andreasreiterer.at/single-result-with-promise-all/
 
-let doAThing = () => {
-  alert("hello");
-};
-
-document.getElementById("getWordsButton").addEventListener("click", () => {
-  getWords();
-});
 /**
  * Part 1. Fetch words from WordsAPI.
  */
 
 // Create the basic URL we will fetch. We are using a function because we might want to change these values dynamically.
 url = (wordLength, partOfSpeech) => {
-  return `https://wordsapiv1.p.rapidapi.com/words/?letters=${wordLength}&partOfSpeech=${partOfSpeech}&limit=1&random=true`;
+  return `https://wordsapiv1.p.rapidapi.com/words/?letters=${wordLength}&partOfSpeech=${partOfSpeech}&limit=1&letterPattern=\\S&random=true`;
 };
 
 // WordsAPI requires us to pass this info, so let's create an object for it to consume.
@@ -28,60 +19,99 @@ const options = {
 };
 
 // Create an array of requests we want to fetch.
-// We use an array because later we might want to make a dynamic number of calls.
-const requestsToFetch = [
-  fetch(new Request(url("4", "adjective"), options)),
-  fetch(new Request(url("4", "noun"), options))
+const urls = [
+  new Request(url("4", "adjective"), options),
+  new Request(url("4", "noun"), options)
 ];
 
-// Promise all. Let's wait till we have all our info back to do anything.
-let getWords = () => {
-  Promise.all(requestsToFetch)
-    .then(results => {
-      // When the array of results come back, go through them one at a time and run them through the processResult function.
-      results.forEach(result => {
-        // The fetch returns a promise containing an object. We want to parse it into JSON.
-        processResult(result.json());
-      });
+// Fetch all our URLs and return them once they've all come through.
+let fetchData = () => {
+  const allRequests = urls.map(url =>
+    fetch(url).then(async response => {
+      // Check if the response came back 200-299
+      if (response.ok) {
+        try {
+          return response.json();
+        } catch (error) {
+          return Promise.reject(
+            new ResponseError("Invalid JSON: " + error.message)
+          );
+        }
+      }
+      // If the reponse is NOT ok, let's do something else entirely.
+      else {
+        fingers404();
+      }
     })
-    .catch(err => {
-      console.error("The fetch didn't return correctly: ");
-      throw err;
-    });
+  );
+
+  return Promise.all(allRequests);
 };
 
-// Each result.json() comes in as the argument, which we have called resultPromise
-let processResult = resultPromise => {
-  // The second .then takes the resultPromise, turns it into usable data, and sends it on!
-  resultPromise.then(processedData => {
-    appendOutput(processedData);
-  });
+// @TODO: Make a fun 404 page, or just maybe return some default finger text.
+let fingers404 = () => {
+  // Fill the output with a sorry message.
+  console.log("Sad fingers. 404");
+  outputParent.innerHTML =
+    "<h1>Sad Fingers<br> >__< <br>The data source isn't playing today.</h1>";
 };
+
+// Trigger fetch command
+function getWords() {
+  fetchData().then(arrayOfResponses => {
+    appendOutput(arrayOfResponses);
+  });
+}
+document.getElementById("getWordsButton").addEventListener("click", getWords);
 
 /**
  * Part 2. Display results
  */
 
 // Let's take our processed data, pull out the bits we want, and attach them to the page by calling a few more functions.
-let appendOutput = processedData => {
-  console.log(processedData);
+let appendOutput = responseData => {
+  console.log("appendOutput", responseData);
+  let outputRow = createNode("div");
+  append(outputParent, outputRow);
 
-  // We are putting our words in p tags for now, so let's create some.
-  let p = createNode("p");
-
-  // Put the word we received into the p tag we just made.
-  p.innerHTML = processedData.word;
-
-  // Stick that tag onto the page!
-  append(p);
+  // @TODO: Separate the words into left fist and right fist based on part of speech, since we should not need to know the order in which the objects came back.
+  responseData.forEach(datum => {
+    // Check if anything came back. If not, ignore.
+    if (datum) {
+      // Create an element to put our word in.
+      let rowCell = createNode("span");
+      // If our query returned a word, put it in the element.
+      if (datum.word) {
+        rowCell.innerHTML = datum.word;
+      }
+      // If there is an error with the query and the object comes back without a word, we will hardcode one!
+      else {
+        rowCell.innerHTML = "oops";
+      }
+      // Stick that tag onto the page!
+      append(outputRow, rowCell);
+    }
+  });
 };
 
-outputParent = document.getElementById("output");
+/**
+ * Helpers
+ */
+
+//  Places where we keep the output on the page
+outputParent = document.getElementById("outputHistory");
 
 function createNode(element) {
   return document.createElement(element);
 }
 
-function append(el) {
-  return outputParent.appendChild(el);
+function append(parent, el) {
+  return parent.appendChild(el);
 }
+
+// function append(el) {
+//   return outputParent.appendChild(el);
+// }
+
+// JUST FOR DEV, SO WE CAN RUN WITHOUT PRESSING ANY BUTTONS
+getWords();
